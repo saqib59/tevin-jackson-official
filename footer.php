dcdb8963a52d247aec19a818c2b7210480be5dd0
Saqib Ali <saqibali80400@gmail.com>
2025-03-07 15:45:59 +0500

    first commit

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Flipping_Detroit
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'flipping-detroit' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'flipping-detroit' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s', 'flipping-detroit' ), 'flipping-detroit', );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ home-template.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Home
+ */
+
+get_header(); ?>
+
+<main id="main-content">
+    <section class="hero">
+        <h1>Welcome to My Custom Home Page</h1>
+        <p>This is a home page template.</p>
+    </section>
+
+    <section class="content">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post();
+                the_content();
+            endwhile;
+        endif;
+        ?>
+    </section>
+</main>
+
+<?php get_footer(); ?>
